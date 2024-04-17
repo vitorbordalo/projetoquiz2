a7f64fb40b30a93b864ef3019aa22533839d5832
vitorbordalo <151081737+vitorbordalo@users.noreply.github.com>
2024-04-17 10:51:20 -0300

    feat: adciona perguntas aleatorias

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+
+    <template v-if="this.question">
+  
+        <h1 v-html="this.question">
+        </h1>
+  
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input 
+          type="radio" 
+          name="options" 
+          value="answer"
+          v-model="this.chosen_answer"
+          >
+    
+        <label v-html="answer"></label><br>
+      </template>
+        
+      <button class="send" type="button">Send</button>
+
+    </template>
+    
+  </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosen_answer: undefined
+    }
+  },
+  
+  computed: {
+    answers() {
+      var answers = [...this.incorrectAnswers];
+      answers.splice( Math.round(Math.random() * answers.length) , 0, this.correctAnswer);
+      return answers;
+    }
+  },
+  created() {
+    this.axios
+    .get('https://opentdb.com/api.php?amount=1&category=18')
+    .then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+    })
   }
+
 }
+//https://opentdb.com/api.php?amount=1&category=18
+
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+   
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(VueAxios, axios)
+.mount('#app')
+
